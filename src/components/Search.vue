ad8801f2ce53096a88bca6e905d9ee4b87568e5f
Fazendaaa <lucas.carotta@outlook.com>
2022-09-11 22:00:18 -0300

    Improving search tab

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,16 +6,13 @@
     </v-card-title>
     <v-card-text>
       <v-subheader class="pa-0">
-        Look for something familiar, then go from the from there
+        Just release your imagination, find something interesting here
       </v-subheader>
       <v-autocomplete
         v-model="model"
-        :hint="!isEditing ? 'Click the icon to edit' : 'Click the icon to save'"
-        :items="states"
-        :readonly="!isEditing"
-        :label="`State — ${isEditing ? 'Editable' : 'Readonly'}`"
+        :label="`Search here`"
         persistent-hint
-        prepend-icon="mdi-city"
+        prepend-icon="mdi-magnify"
       >
         <template v-slot:append-outer>
           <v-slide-x-reverse-transition mode="out-in">
@@ -43,67 +40,6 @@ export default defineComponent({
     return {
       isEditing: false,
       model: null,
-      states: [
-        "Alabama",
-        "Alaska",
-        "American Samoa",
-        "Arizona",
-        "Arkansas",
-        "California",
-        "Colorado",
-        "Connecticut",
-        "Delaware",
-        "District of Columbia",
-        "Federated States of Micronesia",
-        "Florida",
-        "Georgia",
-        "Guam",
-        "Hawaii",
-        "Idaho",
-        "Illinois",
-        "Indiana",
-        "Iowa",
-        "Kansas",
-        "Kentucky",
-        "Louisiana",
-        "Maine",
-        "Marshall Islands",
-        "Maryland",
-        "Massachusetts",
-        "Michigan",
-        "Minnesota",
-        "Mississippi",
-        "Missouri",
-        "Montana",
-        "Nebraska",
-        "Nevada",
-        "New Hampshire",
-        "New Jersey",
-        "New Mexico",
-        "New York",
-        "North Carolina",
-        "North Dakota",
-        "Northern Mariana Islands",
-        "Ohio",
-        "Oklahoma",
-        "Oregon",
-        "Palau",
-        "Pennsylvania",
-        "Puerto Rico",
-        "Rhode Island",
-        "South Carolina",
-        "South Dakota",
-        "Tennessee",
-        "Texas",
-        "Utah",
-        "Vermont",
-        "Virgin Island",
-        "Virginia",
-        "Washington",
-        "West Virginia",
-        "Wisconsin",
-        "Wyoming",
-      ],
     };
   },
 });
