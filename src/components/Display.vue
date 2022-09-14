58ed445ababab125e9af6aa84f5f3ed4e1a616e9
Fazendaaa <lucas.carotta@outlook.com>
2022-09-14 11:21:06 -0300

    Adding carousel

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -39,63 +39,73 @@
         </v-tooltip>
       </v-layout>
 
-      <v-layout row wrap>
-        <v-flex
-          xs12
-          sm6
-          md4
-          lg3
-          v-for="subject in subjects"
-          :key="subject.title"
-        >
-          <v-card
-            flat
-            class="text-xs-center ma-3 `pa-3 manga ${subject.status}`"
-          >
-            <v-responsive class="pt-4">
-              <v-avatar size="180" class="grey lighten-2">
-                <img :src="subject.cover" :width="size" :height="size" />
-              </v-avatar>
-            </v-responsive>
-            <v-card-text>
-              <div class="subheading">{{ subject.title }}</div>
-              <div class="grey--text">{{ subject.chapters }}</div>
-              <div class="center">
-                <v-chip
-                  small
-                  :class="`${subject.status} white--text my-2 caption`"
-                  >{{ subject.status }}</v-chip
-                >
-              </div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
+      <carousel :items-to-show="1">
+        <slide v-for="slide in 10" :key="slide">
+          <v-layout row wrap>
+            <v-flex
+              xs12
+              sm6
+              md4
+              lg3
+              v-for="subject in subjects"
+              :key="subject.title"
+            >
+              <v-card
                 flat
-                color="grey"
-                :to="{ name: 'Info', params: { id: subject.title } }"
-                custom
+                class="text-xs-center ma-3 `pa-3 manga ${subject.status}`"
               >
-                <v-icon small left>mdi-book-open-variant</v-icon>
-                <span>Read</span>
-              </v-btn>
-              <v-btn
-                flat
-                color="grey"
-                v-on:click="addToReadlist(subject.title)"
-              >
-                <v-icon small left>mdi-bookmark-plus-outline</v-icon>
-                <span class="">Add to Readlist</span>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
+                <v-responsive class="pt-4">
+                  <v-avatar size="180" class="grey lighten-2">
+                    <img :src="subject.cover" :width="size" :height="size" />
+                  </v-avatar>
+                </v-responsive>
+                <v-card-text>
+                  <div class="subheading">{{ subject.title }}</div>
+                  <div class="grey--text">{{ subject.chapters }}</div>
+                  <div class="center">
+                    <v-chip
+                      small
+                      :class="`${subject.status} white--text my-2 caption`"
+                      >{{ subject.status }}</v-chip
+                    >
+                  </div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn
+                    flat
+                    color="grey"
+                    :to="{ name: 'Info', params: { id: subject.title } }"
+                    custom
+                  >
+                    <v-icon small left>mdi-book-open-variant</v-icon>
+                    <span>Read</span>
+                  </v-btn>
+                  <v-btn
+                    flat
+                    color="grey"
+                    v-on:click="addToReadlist(subject.title)"
+                  >
+                    <v-icon small left>mdi-bookmark-plus-outline</v-icon>
+                    <span class="">Add to Readlist</span>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </slide>
+        <template #addons>
+          <navigation />
+          <pagination />
+        </template>
+      </carousel>
     </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 
 interface Subject {
   title: string;
@@ -107,6 +117,13 @@ interface Subject {
 
 export default defineComponent({
   name: "DisplayComponent",
+
+  components: {
+    Carousel,
+    // Slide,
+    Pagination,
+    Navigation,
+  },
 
   props: {
     header: String,
